d6888f4c6ef61255325a71a908c083ead310e8a5
Sushil <sushilmangal731@gmail.com>
2021-01-13 12:51:05 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const menubar = document.getElementById('menubar')
+let scrolled = false
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    menubar.classList.remove('top')
+    if (!scrolled) {
+      menubar.style.transform = 'translateY(-70px)'
+    }
+    setTimeout(function () {
+      menubar.style.transform = 'translateY(0)'
+      scrolled = true
+    }, 200)
+  } else {
+    menubar.classList.add('top')
+    scrolled = false
+  }
+}
+
+// Smooth Scrolling
+// $('#menubar a, .btn').on('click', function (e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top - 100,
+//       },
+//       800
+//     );
+//   }
+// });
